a5145f8345084f8adfc7bf22e59d5a0cda2329a4
Kryniek <shasu93@gmail.com>
2019-04-29 22:36:52 +0200

    BrowserRouter

--- src/component/App.js
+++ src/component/App.js
@@ -1,58 +1,16 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import ToDo from './ToDo';
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      users: [],
-      isLoading: true,
-      isError: false
-    }
-  }
-
-  //kiedy komponent dodawany jest do drzewka DOM
-  componentWillMount() {
-    //przed render
-  }
-
-  componentDidMount() {
-    //po render
-  }
-
-  //kiedy komponent jest aktualizowany
-  componentWillReceiveProps(nextProps) {
-    //po zmianie propsów przez rodzica
-  }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    //wywoływane przez render
-  }
-
-  componentWillUpdate(nextProps, nextState) {
-    //przed render
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    //po render
-  }
-
-  //kiedy komponent jest odmontowywany
-  componentWillUnmount() {
-    //...
-  }
-
-  //kiedy pojawi się błąd podczas renderowania
-  componentDidCatch(error, info) {
-    //...
-  }
-
-  //zawsze oprócz odmontowywania komponentu
   render() {
     return (
-      <div className="App">
-        <h1>Hello World!</h1>
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ToDo}></Route>
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
