874aeef7ef570dbd1d8bd770f44ea2a3fbf353da
Kryniek <shasu93@gmail.com>
2019-04-29 22:05:41 +0200

    ComponentMethods

--- src/component/App.js
+++ src/component/App.js
@@ -1,6 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class App extends React.Component {
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      users: [],
+      isLoading: true,
+      isError: false
+    }
+  }
+
+  //kiedy komponent dodawany jest do drzewka DOM
+  componentWillMount() {
+    //przed render
+  }
+
+  componentDidMount() {
+    //po render
+  }
+
+  //kiedy komponent jest aktualizowany
+  componentWillReceiveProps(nextProps) {
+    //po zmianie propsów przez rodzica
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    //wywoływane przez render
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    //przed render
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    //po render
+  }
+
+  //kiedy komponent jest odmontowywany
+  componentWillUnmount() {
+    //...
+  }
+
+  //kiedy pojawi się błąd podczas renderowania
+  componentDidCatch(error, info) {
+    //...
+  }
+
+  //zawsze oprócz odmontowywania komponentu
   render() {
     return (
       <div className="App">
@@ -9,12 +56,3 @@ class App extends React.Component {
     );
   }
 }
-// function App() {
-//   return (
-//     <div className="App">
-//       <h1>Hello World!</h1>
-//     </div>
-//   );
-// }
-
-export default App;
