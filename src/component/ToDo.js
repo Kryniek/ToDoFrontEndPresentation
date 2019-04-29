8405eb9f85249735897c441b5b54ef3ac8430aba
Kryniek <shasu93@gmail.com>
2019-04-29 22:37:30 +0200

    BrowserRouter 2

---
+++ src/component/ToDo.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class ToDo extends Component {
+    render() {
+        return (
+            <div className="ToDoComponent">
+                <h1>Hello World!</h1>
+            </div>
+        );
+    }
+}
